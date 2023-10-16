e09b0a07fe1a5c749300f1a81cd8763509d70242
jorge mario gil duarte <jmariogild88@gmail.com>
2023-10-15 19:01:13 -0500

    Creación de la rama project-3 donde se actualizo parte del código del proyecto

--- src/list-edit-router.js
+++ src/list-edit-router.js
@@ -1,49 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const validateHTTPMethod = require('./http-method-validator'); // Importa el middleware
 
 module.exports = function (tasks) {
+  // Middleware para manejar errores de solicitud POST y PUT
+  const handleErrors = (req, res, next) => {
+    if (req.method === 'POST' && (!req.body || Object.keys(req.body).length === 0)) {
+      return res.status(400).json({ message: 'El cuerpo de la solicitud POST está vacío' });
+    }
+
+    if (req.method === 'POST' && (!req.body.id || !req.body.otroAtributo)) {
+      return res.status(400).json({ message: 'Información no válida o atributos faltantes para crear la tarea' });
+    }
+
+    if ((req.method === 'PUT' || req.method === 'PATCH') && (!req.body || Object.keys(req.body).length === 0)) {
+      return res.status(400).json({ message: 'El cuerpo de la solicitud PUT/PATCH está vacío' });
+    }
+
+    if ((req.method === 'PUT' || req.method === 'PATCH') && (!req.body.id || !req.body.otroAtributo)) {
+      return res.status(400).json({ message: 'Información no válida o atributos faltantes para actualizar la tarea' });
+    }
+
+    next(); // Continuar con la solicitud si no se encontraron errores
+  };
+
+  // Aplicar el middleware a nivel de aplicación
+  router.use(validateHTTPMethod);
+
+  // Aplicar el middleware a todas las rutas
+  router.use(handleErrors);
+
   // Ruta para crear una tarea (POST)
-  router.post('/crear', (req, res) => {
-    // Inicializa la variable `newTask` con el objeto de la nueva tarea
-    const newTask = { ...req.body };
+router.post('/crear', (req, res) => {
+  // Resto del código para crear una tarea
+  const newTask = { ...req.body };
 
-    // Verifica si la nueva tarea tiene un ID válido
-    if (!newTask.id) {
-      res.status(400).json({ message: 'El ID de la tarea es obligatorio' });
-      return;
-    }
+  // Verifica si la nueva tarea tiene un ID válido
+  if (!newTask.id) {
+    res.status(400).json({ message: 'El ID de la tarea es obligatorio' });
+    return;
+  }
 
-    // Agrega la nueva tarea a la lista de tareas
-    tasks.push(newTask);
+  // Agrega la nueva tarea a la lista de tareas
+  tasks.push(newTask);
 
-    // Responde con la nueva tarea
-    res.json(newTask);
-  });
+  // Responde con la nueva tarea
+  res.json(newTask);
+});
 
-  // Ruta para eliminar una tarea por ID (DELETE)
-  router.delete('/eliminar/:id', (req, res) => {
-    const taskId = req.params.id;
-    const taskIndex = tasks.findIndex(task => task.id === taskId);
-    if (taskIndex !== -1) {
-      tasks.splice(taskIndex, 1);
-      res.json({ message: 'Tarea eliminada con éxito' });
-    } else {
-      res.status(404).json({ message: 'Tarea no encontrada' });
-    }
-  });
+// Ruta para eliminar una tarea por ID (DELETE)
+router.delete('/eliminar/:id', (req, res) => {
+  const taskId = req.params.id;
+  const taskIndex = tasks.findIndex(task => task.id === taskId);
+  if (taskIndex !== -1) {
+    tasks.splice(taskIndex, 1);
+    res.json({ message: 'Tarea eliminada con éxito' });
+  } else {
+    res.status(404).json({ message: 'Tarea no encontrada' });
+  }
+});
 
-  // Ruta para actualizar una tarea por ID (PUT o PATCH)
-  router.put('/actualizar/:id', (req, res) => {
-    const taskId = req.params.id;
-    const updatedTask = req.body; // Se espera un objeto con los detalles actualizados de la tarea
-    const taskIndex = tasks.findIndex(task => task.id === taskId);
-    if (taskIndex !== -1) {
-      tasks[taskIndex] = updatedTask;
-      res.json(updatedTask);
-    } else {
-      res.status(404).json({ message: 'Tarea no encontrada' });
-    }
-  });
+// Ruta para actualizar una tarea por ID (PUT o PATCH)
+router.put('/actualizar/:id', (req, res) => {
+  const taskId = req.params.id;
+  const updatedTask = req.body; // Se espera un objeto con los detalles actualizados de la tarea
+  const taskIndex = tasks.findIndex(task => task.id === taskId);
+  if (taskIndex !== -1) {
+    tasks[taskIndex] = updatedTask;
+    res.json(updatedTask);
+  } else {
+    res.status(404).json({ message: 'Tarea no encontrada' });
+  }
+});
+
 
   return router;
 };
--- src/list-view-router.js
+++ src/list-view-router.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = function (tasks) {
+  // Middleware para validar parámetros
+  const validateParams = (req, res, next) => {
+    const param = req.params.validate;
+
+    // Verifica si el parámetro es válido
+    if (!isValid(param)) {
+      return res.status(400).send('Parámetro no válido');
+    }
+
+    next(); // Continuar con la solicitud si el parámetro es válido
+  };
+
   // Ruta para listar las tareas completas
   router.get('/completas', (req, res) => {
     const completedTasks = tasks.filter(task => task.isCompleted);
@@ -14,5 +26,22 @@ module.exports = function (tasks) {
     res.json(incompleteTasks);
   });
 
+  // Aplicar el middleware a rutas que requieran validación de parámetros
+  router.get('/ruta-con-parametro/:validate', validateParams);
+
   return router;
 };
+
+// Función para validar el parámetro (personalízala según tus necesidades)
+function isValid(param) {
+  // Aquí se puede definir los criterios de validación
+  if (param.length >= 5 && param.length <= 20) {
+    // Verificar si la longitud del parámetro está entre 5 y 20 caracteres
+    return true;
+  }
+
+  // Agrega más criterios de validación según las necesidades
+  // Por ejemplo, verificar si el parámetro contiene solo caracteres alfanuméricos
+
+  return false;
+}
